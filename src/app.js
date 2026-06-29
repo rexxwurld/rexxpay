addcabb0ac957e8e15adb03bdab4a5311fefa858
Emmanuel Edidiong Noah <rexxwurld@gmail.com>
2026-06-29 09:07:49 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
+// CORE MIDDLEWARE
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 app.use(cors({
-    origin: [
-        "http://rexxpay.onrender.com",
-    ],
-    credentials: true 
+    origin: "https://rexxpay.onrender.com",
+    credentials: true
 }));
-
 
 
 const authRoutes = require("./modules/auth/auth.routes");
@@ -25,8 +24,10 @@ app.use("/api/v1/wallet", walletRoutes);
 app.use("/api/v1/transaction", transactionRoutes);
 
 
-app.get("/", (req, res) => {
-    res.send("RexxPay API Running...");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 module.exports = app;
